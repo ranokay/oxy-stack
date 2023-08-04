066855ada7a3acccbccdb29779d358498a6638c4
Yuuta <ranokay@oxystack.com>
2023-08-05 01:30:42 +0300

    clean up

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
-import { withContentlayer } from "next-contentlayer";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		appDir: true,
-		mdxRs: true,
-	},
-};
+	pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+	// experimental: {
+	// 	appDir: true,
+	// },
+}
 
-export default withContentlayer(nextConfig);
+export default nextConfig
