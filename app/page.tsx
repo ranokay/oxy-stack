d75d0f150cf0e1aa88a91d6c53023895b7542d73
Yuuta <61791392+ranokay@users.noreply.github.com>
2023-08-06 00:53:54 +0300

    fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const Home = () => {
 				quantity={100}
 			/>
 			<h1 className='z-10 text-4xl text-transparent duration-1000 bg-gray-200 cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text '>
-				Comming Soon
+				Coming Soon
 			</h1>
 		</div>
 	)
