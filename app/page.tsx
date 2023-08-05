eb6051e15c2a8fbd8d47f8a4aade85f59b5cabb5
Yuuta <ranokay@oxystack.com>
2023-08-06 01:08:30 +0300

    remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Particles from './components/particles'
 const Home = () => {
 	return (
 		<div className='flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black'>
-			<div className='hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0' />
 			<Particles
 				className='absolute inset-0 -z-10 animate-fade-in'
 				quantity={100}
